bce62307f2aa94ed8ec59ae9d59b0f621c333045
Dj Lewis <djlewis2@uno.edu>
2025-04-17 10:24:20 -0700

    Testing github actions

--- discordbot.py
+++ discordbot.py
@@ -46,7 +46,7 @@ async def traffic(ctx, *, args: str = None):
     data = fetch_traffic(origin, destination)
     await ctx.send(f'🚗 Traffic from {origin} to {destination}: {data}')
 
-@tasks.loop(minutes=5)
+@tasks.loop(hours=5)
 async def traffic_update_loop():
     channel = bot.get_channel(CHANNEL_ID)
     if channel:
